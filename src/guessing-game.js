afed2e960a152a1f2d977fcf796f4ba089d6fae2
Tatyana Bukzheleva <ledimersedes@yandex.ru>
2017-02-02 18:55:11 +0300

    C:/Program Files/Git/guessing-game/all

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.arr = [];
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
+    	for (min = 0; min <= max ; min++){
+    		this.arr[min] = min;
+    	}
+    	
     }
 
     guess() {
-
-    }
+    	this.current = Math.ceil((this.max+this.min)/2);
+    	return this.current;
+    	     }
 
     lower() {
-
+    	this.max = this.current;
+    	return this.max;
     }
 
     greater() {
-
+    	this.min = this.current;
+    	return this.min;
     }
 }
 
 module.exports = GuessingGame;
+
